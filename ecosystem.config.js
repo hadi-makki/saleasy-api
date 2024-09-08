e5b3fc187715823a67d57a45826862a5f0292d37
hadimakki387 <hmakki387@gmail.com>
2024-09-08 09:25:39 +0300

    fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: 'saleasy-api',
       script: 'yarn',
-      args: 'start',
-      cwd: 'saleasy-api',
-      watch: true,
+      args: 'start:prod', // Start the app in production mode
+      cwd: './saleasy-api', // Ensure this is the correct path to your NestJS app
+      watch: false, // It's common not to watch files in production
       env: {
         NODE_ENV: 'production',
       },
