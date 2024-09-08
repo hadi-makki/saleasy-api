4fdcaac79fcc5a11c25ed15f33e93a29aa1c7a48
hadimakki387 <hmakki387@gmail.com>
2024-09-08 09:16:47 +0300

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'saleasy-api',
-      script: './dist/main.js',
-      cwd: '/home/ubuntu/saleasy-api',
+      script: 'yarn',
+      args: 'start',
+      cwd: 'saleasy-api',
       watch: true,
       env: {
         NODE_ENV: 'production',
